e3c366a5df9973747cd094deff4e43e2a65137d4
masanorinakamura-09 <gabugabu06@gmail.com>
2024-04-19 11:11:43 +0900

    登録済み顧客情報表示機能の作成

--- src/main/java/com/ec/controller/CustomerController.java
+++ src/main/java/com/ec/controller/CustomerController.java
@@ -1,5 +1,28 @@
 package com.ec.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ec.service.CustomerDetail;
+import com.ec.service.CustomerService;
+
+@Controller
+@RequestMapping("customer")
 public class CustomerController {
+    private final CustomerService customerservice;
+
+    public CustomerController(CustomerService customerservice) {
+        this.customerservice = customerservice;
+    }
+
+    @GetMapping("/detail/")
+    public String getCustomerDetail(@AuthenticationPrincipal CustomerDetail customerdetail,Model model) {
+        model.addAttribute("customer",customerdetail.getCustomer());
+        return "ECommerce/customer";
+    }
+
 
 }
